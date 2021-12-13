17d1bec93ecd7aedd7ff3f5d40756edc970d25d6
Danny Fuentes <danny@Dannys-MacBook-Air.local>
2021-12-13 02:27:04 -0300

    created read your writes

--- servers/broker/broker.go
+++ servers/broker/broker.go
@@ -12,7 +12,37 @@ import (
 	"src/common"
 	// "src/servers/fulcrum"
 )
+func checkVector(x int, y int, z int){
+	s1 := rand.NewSource(time.Now().UnixNano())	
+	r1 := rand.New(s1)
+	if x == y && x == z {
+		rand := r1.Intn(3)
+		return rand
 
+	} else if x == y && x > z{
+		rand := r1.Intn(2)
+
+	} else if x == z && x > y{
+		rand := r1.Intn(2)
+		if rand == 0{
+			return 0
+		} else {
+			return 2
+		}
+
+	} else if y == z && x < y {
+		rand := r1.Intn(2) + 1
+
+	} else if x > y && x > z {
+		return 0
+
+	} else if y > x && y > z {
+		return 1
+
+	} else { //z > x && z > y
+		return 2
+	}
+}
 // func ConnectFulcrum (mensaje string) string{
 // 	var ipFulcrum string = common.Get_env_var("IP_SERVER_20")
 // 	var portFulcrum string = common.Get_env_var("FULCRUM_PORT")
@@ -48,17 +78,17 @@ func (s * Server) RequestConnectionInf(stream BrokerService_RequestConnectionInf
 
 	s1 := rand.NewSource(time.Now().UnixNano())	
 	r1 := rand.New(s1)
-	rand := r1.Intn(3)
+	election := r1.Intn(3)
 	
 	var ip string	
-	
-	if rand == 0{
+	//election = checkVector (x, y, z)
+	if election == 0{
 		ip = common.Get_env_var("IP_SERVER_18")
 
-	} else if rand == 1{
+	} else if election == 1{
 		ip = common.Get_env_var("IP_SERVER_19")
 
-	} else if rand == 2{
+	} else if election == 2{
 		ip = common.Get_env_var("IP_SERVER_20")
 	}
 	// send response
@@ -72,14 +102,16 @@ func (s * Server) RequestConnectionInf(stream BrokerService_RequestConnectionInf
 func (s * Server) RequestConnectionLeia(stream BrokerService_RequestConnectionLeiaServer) error {
 
 	// receibe message
-	// msg, err := stream.Recv()
-	// mensaje := msg.Request
-	// common.Check_error(err, "Error receiving message")
-	
-	// // send response
-	// err = stream.Send(&BrokerResponse{
-	// 	Response: ConnectFulcrum(mensaje),
-	// })
-	// common.Check_error(err, "Error sending response")
+	req, err := stream.Recv()
+	peticion := req.Request
+	common.Check_error(err, "Error receiving message")
+	log.Printf("Request: %s\n", peticion)
+	var answer string = "0"
+	//answer = ConnectFulcrum (mensaje string)
+
+	err = stream.Send(&BrokerResponse{
+		Response: answer,
+	})
+	common.Check_error(err, "Error sending response")
 	return nil
 }
